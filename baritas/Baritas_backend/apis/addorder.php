1a09fcd858edba49611ae10026b3d14318cb946d
kwabena-Koranteng <kwabena.obeng@ashesi.edu.gh>
2021-11-18 06:38:35 +0000

    orders almost done

--- baritas/Baritas_backend/apis/addorder.php
+++ baritas/Baritas_backend/apis/addorder.php
@@ -6,23 +6,40 @@ header('Access-Control-Allow-Headers:Access-Control-Allow-Headers, Content-Type,
 
 include_once '../../Baritas_backend/database/Database.php';
 include_once '../../Baritas_backend/Model/orders.php';
+include_once '../../Baritas_backend/Model/order_items.php';
 
 $database = new Database();
 $db = $database->connect();
-$order = new orders($db);
 
+$order = new orders($db);
+$items  = new order_items($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-$order->date=$data->d;
-$order->pay=$data->pa;
+$order->date=$data->dat;
+$order->pay=$data->payment;
 $order->waiter= $data->wait;
 $order->total_cost= $data->cost;
-$order->stats=$data->stat;
+$order->stats=$data->status;
+$order->restaurant = $data->rest;
+$prder->table = $data->table;
+$order->dine = $data->type;
+$order->user_id = $data->cashier;
+$order->sub =$data->s;
+$data->notes =$data->special_notes;
 
-if($order->create()){
-   echo true;
+$order->create();
+
+$list = $data->orderList;
+
+foreach($list as $p){
+    $items->order = $order->getId();
+    $items->menu = $p->menu_id;
+    $items->quant = $p->quantity;
+    $items->atm = $p->amount;
+
+    $item->create();
 }
-else{
-    echo false;
-}
+
+
+?>
