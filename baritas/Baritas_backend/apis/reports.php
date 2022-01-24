1c717b35c455544774072491fb166ef304b57927
kwabena-Koranteng <kwabena.obeng@ashesi.edu.gh>
2022-01-24 11:52:12 +0000

    WIP

--- baritas/Baritas_backend/apis/reports.php
+++ baritas/Baritas_backend/apis/reports.php
@@ -2,7 +2,6 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-
 include_once '../../Baritas_backend/database/Database.php';
 include_once '../../Baritas_backend/Model/orders.php';
 
